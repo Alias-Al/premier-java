4705518a1212f69646f4fc9edfaf586016f4ed56
Alias-Al <thecondemned.1@gmail.com>
2023-12-14 17:31:45 +0100

    mon cul sur la comode

---
+++ testgi.java
@@ -0,0 +1,18 @@
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempsPartiel {
+
+    
+    
+       
+
+
+
+}
+    
+
